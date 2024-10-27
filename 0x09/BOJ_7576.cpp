f9094e4f8b98c55afbea3d66029ccee0901c705f
yulimmm <89307538+yulimmm@users.noreply.github.com>
2024-10-27 13:31:26 +0900

    Update BOJ_7576.cpp

--- 0x09/BOJ_7576.cpp
+++ 0x09/BOJ_7576.cpp
@@ -1,61 +1,77 @@
 #include <iostream>
-#include <queue>
 #include <utility>
+#include <queue>
+#include <algorithm>
 using namespace std;
 
-#define X first
-#define Y second
+int board[1000][1000];
+int dist[1000][1000];
+int dx[4] = {1,0,-1,0};
+int dy[4] = {0,1,0,-1};
+int n,m;
 
-int board[1002][1002];
-int dist[1002][1002];
-int dx[4] = { 1,0,-1,0 };
-int dy[4] = { 0,1,0,-1 };
-
-int main() 
+void printdist()
 {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
+  for(int i = 0; i < m; i++){
+    for(int j = 0; j < n; j++){
+      cout<<dist[i][j];
+    }
+    cout<<'\n';
+  }
+}
 
-	int m, n;
-	cin >> m >> n;
+void printboard()
+{
+  for(int i = 0; i < m; i++){
+    for(int j = 0; j < n; j++){
+      cout<<board[i][j];
+    }
+    cout<<'\n';
+  }
+}
 
-	queue<pair<int, int>>Q;
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			cin >> board[i][j];
-			if (board[i][j] == 1) {
-				Q.push({ i,j });
-			}
-			if (board[i][j] == 0) dist[i][j] = -1; //이 부분 확인
-		}
-	}
-
-	while (!Q.empty()) {
-		pair<int, int> cur = Q.front();
-		Q.pop();
-		for (int i = 0; i < 4; i++) {
-			int nx = cur.X + dx[i];
-			int ny = cur.Y + dy[i];
-			if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
-			if (dist[nx][ny] >= 0) continue;
-			dist[nx][ny] = dist[cur.X][cur.Y] + 1;
-			Q.push({ nx,ny });
-		}
-	}
-
-	int max = 0;
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			if (dist[i][j] == -1) {
-				cout << -1;
-				return 0;
-			}
-			if (dist[i][j] > max) {
-				max = dist[i][j];
-			}
-		}
-	}
-	cout << max;
-
-	return 0;
+int main()
+{
+  ios::sync_with_stdio(0); cin.tie(0);
+  cin >> n >> m;
+  for(int i = 0; i < m; i++) fill(dist[i], dist[i]+n, -1);
+  queue<pair<int, int>>Q;
+  for(int i = 0; i < m; i++){
+    for(int j = 0; j < n; j++){
+      cin >> board[i][j];
+      if(board[i][j]==1) {
+        Q.push({i,j});
+        dist[i][j] = 0;
+      }
+      if(board[i][j]==-1){
+        dist[i][j]=-2;
+      }
+    }
+  }
+  //printdist();
+  while(!Q.empty()){
+    pair<int, int>cur = Q.front(); Q.pop();
+    for(int i = 0; i < 4; i++){
+      int nx = cur.first + dx[i];
+      int ny = cur.second + dy[i];
+      if(nx < 0 || nx >= m || ny < 0 || ny >= n) continue;
+      if(dist[nx][ny]!=-1 || board[nx][ny]==-1) continue;
+      dist[nx][ny] = dist[cur.first][cur.second] + 1;
+      Q.push({nx,ny});
+    }
+  }
+  int ans = 0;
+  bool result = true;
+  for(int i = 0; i < m; i++){
+    for(int j = 0; j < n; j++){
+      ans = max(ans, dist[i][j]);
+      if(dist[i][j]==-1) result = false;
+    }
+  }
+  //printdist();
+  //cout<<"\n-------------\n";
+  //printboard();
+  if(!result) cout<<-1;
+  else cout<<ans;
+  return 0;
 }
