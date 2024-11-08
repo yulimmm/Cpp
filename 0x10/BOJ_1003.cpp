87eccf985feb47253d39eeac6a4fc2b7237cfd24
yulimmm <89307538+yulimmm@users.noreply.github.com>
2024-11-08 23:24:56 +0900

    Update BOJ_1003.cpp

--- 0x10/BOJ_1003.cpp
+++ 0x10/BOJ_1003.cpp
@@ -1,27 +1,30 @@
 #include <iostream>
 using namespace std;
 
-int T;
-int N[42];
+int fibo[42][2];
+int main(void) {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
 
-int fibonacci(int n) {
-  if(n == 0){
-    cout<<0;
-    return 0;
-  } 
-  else if (n == 1){
-    cout<<1;
-    return 1;
+  fibo[0][0] = 1;
+  fibo[1][1] = 1;
+  for (int i = 2; i <= 40; i++) {
+    fibo[i][0] = fibo[i - 1][0] + fibo[i - 2][0];
+    fibo[i][1] = fibo[i - 1][1] + fibo[i - 2][1];
   }
-  else {
-    return fibonacci(n-1) + fibonacci(n-2);
-  }
-}
-
-int main()
-{
-  ios::sync_with_stdio(0); cin.tie(0);
+  int T;
   cin >> T;
-
-  return 0;
+  while (T--) {
+    int N;
+    cin >> N;
+    cout << fibo[N][0] << " " << fibo[N][1] << endl;
+  }
 }
+/*
+1. 테이블 정하기
+fibo[i][k] = 숫자 i가 k(0 or 1)를 호출한 횟수
+2. 점화식 찾기
+fibo[i][k] = fibo[i - 1][k] + fibo[i - 2][k]
+3. 초기값 정하기
+fibo[0][0] = 1, fibo[1][1] = 1
+*/
