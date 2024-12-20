6eafbf831cbf450da79bf20b6eb09a366447f097
yulimmm <89307538+yulimmm@users.noreply.github.com>
2024-12-20 15:02:45 +0900

    Update BOJ_2178.cpp

--- 0x09/BOJ_2178.cpp
+++ 0x09/BOJ_2178.cpp
@@ -1,43 +1,36 @@
 #include <iostream>
-#include <string>
 #include <queue>
 #include <utility>
-
 using namespace std;
 
-#define X first
-#define Y second
-
-string board[102];
-int dist[102][102];
 int n,m;
+string board[105];
+int dist[105][105];
 int dx[4] = {1,0,-1,0};
 int dy[4] = {0,1,0,-1};
 
-int main(void){
-  ios::sync_with_stdio(0);
-  cin.tie(0);
+int main()
+{
+  ios::sync_with_stdio(0); cin.tie(0);
   cin >> n >> m;
-  
-  for(int i = 0; i < n; i++)
+  for(int i = 0; i < n; i++){
     cin >> board[i];
-  
-  for(int i = 0; i < n; i++) fill(dist[i],dist[i]+m,-1);
-  
-  queue<pair<int,int> > Q;
-  Q.push({0,0});
-  dist[0][0] = 0;
-  
+    fill(dist[i], dist[i]+m, 0);
+  }
+  queue<pair<int, int>>Q;
+  Q.push({0,0}); dist[0][0] = 1;
   while(!Q.empty()){
-    auto cur = Q.front(); Q.pop();
+    pair<int, int>current = Q.front(); Q.pop();
+    int x = current.first;
+    int y = current.second;
     for(int dir = 0; dir < 4; dir++){
-      int nx = cur.X + dx[dir];
-      int ny = cur.Y + dy[dir];
+      int nx = x + dx[dir];
+      int ny = y + dy[dir];
       if(nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
-      if(dist[nx][ny] >= 0 || board[nx][ny] != '1') continue;
-      dist[nx][ny] = dist[cur.X][cur.Y]+1;
-      Q.push({nx,ny});
+      if(board[nx][ny]=='0' || dist[nx][ny]!=0) continue;
+      Q.push({nx,ny}); dist[nx][ny]=dist[x][y]+1;
     }
   }
-  cout << dist[n-1][m-1]+1; // 문제의 특성상 거리+1이 정답
+  cout<<dist[n-1][m-1];
+  return 0;
 }
