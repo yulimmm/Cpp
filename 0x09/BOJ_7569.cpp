bf2f5428777a14d8886b6c5bbaba208f84085226
yulimmm <89307538+yulimmm@users.noreply.github.com>
2024-12-28 15:11:54 +0900

    Update BOJ_7569.cpp

--- 0x09/BOJ_7569.cpp
+++ 0x09/BOJ_7569.cpp
@@ -1,66 +1,85 @@
 #include <iostream>
-#include <utility>
 #include <queue>
-#include <array>
 #include <algorithm>
 using namespace std;
-
-int board[100][100][100];
-int dist[100][100][100] = {0};
+int board[102][102][102];
+int dist[102][102][102];
 int dx[6] = {1,0,0,-1,0,0};
 int dy[6] = {0,1,0,0,-1,0};
 int dz[6] = {0,0,1,0,0,-1};
+struct XYZ{
+  int x;
+  int y;
+  int z;
+};
+int m,n,h,ans=0;
+queue<XYZ>Q;
+
+void printDist()
+{
+  for(int k = 0; k < h; k++){
+    for(int j = 0; j < n; j++){
+      for(int i = 0; i < m; i++){
+        cout<<dist[k][j][i];
+      }
+      cout<<'\n';
+    }
+    cout<<'\n';
+  }
+}
 
 int main()
 {
   ios::sync_with_stdio(0); cin.tie(0);
-  int X, Y, Z;
-  cin >> X >> Y >> Z;
-  queue<array<int,3>>Q;
-  for(int z = 0; z < Z; z++){
-    for(int y = 0; y < Y; y++){
-      for(int x = 0; x < X; x++){
-        cin >> board[z][y][x];
-        if(board[z][y][x] == 1) {Q.push({x,y,z}); dist[z][y][x]++;}
-        if(board[z][y][x] == -1) dist[z][y][x] = -1;
+  cin >> m >> n >> h;
+  bool pass = 1;
+  for(int k = 0; k < h; k++){
+    for(int j = 0; j < n; j++){
+      for(int i = 0; i < m; i++) {
+        cin >> board[k][j][i];
+        dist[k][j][i] = -1;
+        if(board[k][j][i]==1){
+          XYZ xyz = {i,j,k};
+          Q.push(xyz);
+          dist[k][j][i] = 0;
+        }
+        else if(board[k][j][i]==-1) dist[k][j][i] = -2;
+        else if(board[k][j][i]==0 && pass==1) pass = 0;
       }
     }
   }
-
-  int mn = 0;
-
+  if(pass){
+    cout<<0;
+    return 0;
+  }
   while(!Q.empty()){
-    auto cur = Q.front(); Q.pop();
+    XYZ xyz = Q.front(); Q.pop();
+    int x = xyz.x;
+    int y = xyz.y;
+    int z = xyz.z;
     for(int dir = 0; dir < 6; dir++){
-      int nx = cur[0] + dx[dir];
-      int ny = cur[1] + dy[dir];
-      int nz = cur[2] + dz[dir];
-      if(nx < 0 || nx >= X || ny < 0 || ny >= Y || nz < 0 || nz >= Z) continue;
-      if(dist[nz][ny][nx]!=0 || board[nz][ny][nx]==-1) continue;
-      dist[nz][ny][nx] = dist[cur[2]][cur[1]][cur[0]]+1;
-      Q.push({nx,ny,nz});
-      mn = max(mn, dist[nz][ny][nx]);
+      int nx = x + dx[dir];
+      int ny = y + dy[dir];
+      int nz = z + dz[dir];
+      if(nx < 0 || nx >= m || ny < 0 || ny >= n || nz < 0 || nz >= h) continue;
+      if(dist[nz][ny][nx]>-1 || board[nz][ny][nx]!=0) continue;
+      dist[nz][ny][nx] = dist[z][y][x] + 1;
+      xyz = {nx,ny,nz};
+      Q.push(xyz);
+      ans = max(ans, dist[nz][ny][nx]);
     }
   }
-
-  bool sucess = true;
-
-  //cout<<"result===============\n";
-  for(int z = 0; z < Z; z++){
-    for(int y = 0; y < Y; y++){
-      for(int x = 0; x < X; x++){
-        if(dist[z][y][x]==0) sucess = false;
-        //cout<<dist[z][y][x];
+  bool success = 1;
+  for(int k = 0; k < h; k++){
+    for(int j = 0; j < n; j++){
+      for(int i = 0; i < m; i++){
+        if(dist[k][j][i]==-1) success = 0;
       }
-      //cout<<endl;
     }
-    //cout<<endl;
   }
-  if(sucess){
-    if(mn==0) cout<<0;
-    else cout<<mn-1;
-  }
+  if(success) cout << ans;
   else cout<<-1;
-
+  //cout<<'\n';
+  //printDist();
   return 0;
 }
