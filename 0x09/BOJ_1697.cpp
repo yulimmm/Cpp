b95b509cb18a337edb2e8492f42e5f45493db754
yulimmm <89307538+yulimmm@users.noreply.github.com>
2024-12-28 12:40:26 +0900

    Update BOJ_1697.cpp

--- 0x09/BOJ_1697.cpp
+++ 0x09/BOJ_1697.cpp
@@ -1,27 +1,30 @@
 #include <iostream>
 #include <queue>
-#include <utility>
 using namespace std;
-#define X first
-#define Y second
-int dist[100002];
-int n,k;
-int main(void){
-  ios::sync_with_stdio(0);
-  cin.tie(0);
+int n, k;
+int dist[1000001];
+int dx[2] = {-1,1};
+queue<int>Q;
+
+int main()
+{
+  ios::sync_with_stdio(0); cin.tie(0);
   cin >> n >> k;
-  fill(dist, dist+100001,-1);
-  dist[n] = 0;
-  queue<int> Q;
-  Q.push(n);
-  while(dist[k] == -1){
-    int cur = Q.front(); Q.pop();
-    for(int nxt : {cur-1, cur+1, 2*cur}){
-      if(nxt < 0 || nxt > 100000) continue;
-      if(dist[nxt] != -1) continue;
-      dist[nxt] = dist[cur]+1;
-      Q.push(nxt);
-    }        
+  fill(dist, dist+1000000,-1);
+  Q.push(n); dist[n] = 0;
+  while(!Q.empty()){
+    int current = Q.front(); Q.pop();
+    for(int dir = 0; dir < 3; dir++){
+      int nx;
+      if(dir==2) nx = current*2;
+      else nx = current + dx[dir];
+      if(nx < 0 || nx > 1000000) continue;
+      if(dist[nx]>=0) continue;
+      Q.push(nx);
+      dist[nx] = dist[current] + 1;
+    }
   }
-  cout << dist[k];
+  cout<<dist[k];
+  //printdist();
+  return 0;
 }
