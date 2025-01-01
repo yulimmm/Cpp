ecd3bf00377b564b3f82858d897b55d6a0799af4
yulimmm <89307538+yulimmm@users.noreply.github.com>
2025-01-01 23:25:34 +0900

    Update BOJ_2206.cpp

--- 0x09/BOJ_2206.cpp
+++ 0x09/BOJ_2206.cpp
@@ -1,100 +1,62 @@
 #include <iostream>
-#include <queue>
 #include <utility>
+#include <queue>
+#include <tuple>
 using namespace std;
+#define X first
+#define Y second
 
+int dx[4] = {0,1,0,-1};
+int dy[4] = {1,0,-1,0};
+
+char board[1000][1000];
+int dist[1000][1000][2];
+// dist[x][y][0] : 벽을 하나도 부수지 않고 (x,y)까지 오는데 걸리는 비용
+// dist[x][y][1] : 벽을 하나만 부수고 (x,y)까지 오는데 걸리는 비용, (x,y)가 벽이라서 부수는 경우 포함
 int n, m;
-int dx[4] = {1, 0, -1, 0};
-int dy[4] = {0, 1, 0, -1};
-string orgBoard[1002];
-string tempBoard[1002];
-int dist[1002][1002];
-int ans = 1000000;
-queue<pair<int, int>> walls;
 
-void debugDist() {
-    cout << "\n----Dist----\n";
-    for (int y = 0; y < n; y++) {
-        for (int x = 0; x < m; x++) {
-            cout << dist[y][x];
-        }
-        cout << '\n';
-    }
+bool OOB(int x, int y){
+  return x < 0 || x >= n || y < 0 || y >= m;
 }
 
-void printTempBoard() {
-    cout << "\n----tempBoard----\n";
-    for (int y = 0; y < n; y++) {
-        for (int x = 0; x < m; x++) {
-            cout << tempBoard[y][x];
-        }
-        cout << '\n';
+int bfs() {
+  for (int i = 0; i < n; ++i)
+    for (int j = 0; j < m; ++j)
+      dist[i][j][0] = dist[i][j][1] = -1;
+  dist[0][0][0] = dist[0][0][1] = 1;
+  queue<tuple<int, int, int>> q;
+  q.push({0,0,0});
+  while (!q.empty()) {
+    int x, y, broken;
+    tie(x, y, broken) = q.front();
+    if(x == n-1 && y == m-1) return dist[x][y][broken];
+    q.pop();
+    int nextdist = dist[x][y][broken] + 1;
+    for (int dir = 0; dir < 4; ++dir) {
+      int nx = x + dx[dir];
+      int ny = y + dy[dir];
+      if(OOB(nx, ny)) continue;      
+      if (board[nx][ny] == '0' && dist[nx][ny][broken] == -1) {
+        dist[nx][ny][broken] = nextdist;
+        q.push({nx, ny, broken});
+      }      
+      // (nx, ny)를 부수는 경우
+      if (!broken && board[nx][ny] == '1' && dist[nx][ny][1] == -1) {
+        dist[nx][ny][1] = nextdist;
+        q.push({nx, ny, 1});
+      }      
     }
+  }
+  return -1;
 }
 
-int getDist() {
-    queue<pair<int, int>> Q;
-    Q.push({0, 0});
-    dist[0][0] = 1;
-
-    while (!Q.empty()) {
-        pair<int, int> cur = Q.front();
-        Q.pop();
-        int x = cur.first;
-        int y = cur.second;
-        for (int dir = 0; dir < 4; dir++) {
-            int nx = x + dx[dir];
-            int ny = y + dy[dir];
-            if (nx < 0 || nx >= m || ny < 0 || ny >= n) continue;
-            if (dist[ny][nx] > -1 || tempBoard[ny][nx] == '1') continue;
-            Q.push({nx, ny});
-            dist[ny][nx] = dist[y][x] + 1;
-        }
-    }
-    return (dist[n - 1][m - 1] == -1) ? -1 : dist[n - 1][m - 1];
-}
-
-void Init(int X, int Y) {
-    for (int y = 0; y < n; y++) {
-        for (int x = 0; x < m; x++) {
-            tempBoard[y][x] = orgBoard[y][x];
-            dist[y][x] = -1;
-        }
-    }
-    if (X >= 0 && X < m && Y >= 0 && Y < n) { // 안전하게 범위 확인
-        tempBoard[Y][X] = '0';
-    }
-}
-
-int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-
-    cin >> n >> m;
-    for (int y = 0; y < n; y++) {
-        cin >> orgBoard[y];
-        for (int x = 0; x < m; x++) {
-            if (orgBoard[y][x] == '1') {
-                walls.push({x, y});
-            }
-        }
-    }
-
-    int count = walls.size();
-    Init(0, 0);
-    int temp = getDist();
-    if (temp != -1) ans = temp;
-
-    while (count--) {
-        pair<int, int> brok = walls.front();
-        walls.pop();
-        Init(brok.first, brok.second);
-        temp = getDist();
-        if (temp != -1) ans = min(ans, temp);
-    }
-
-    if (ans == 1000000) cout << -1;
-    else cout << ans;
-
-    return 0;
+int main(void) {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cin >> n >> m;
+  for (int i = 0; i < n; ++i)
+    for (int j = 0; j < m; ++j)
+      cin >> board[i][j];
+  cout << bfs();
+  return 0;
 }
