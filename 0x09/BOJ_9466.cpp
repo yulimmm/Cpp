cfbfc9f50195e0609090967436aaeddd154f72a2
yulimmm <89307538+yulimmm@users.noreply.github.com>
2025-01-06 16:20:21 +0900

    third try

--- 0x09/BOJ_9466.cpp
+++ 0x09/BOJ_9466.cpp
@@ -1,34 +1,50 @@
 #include <iostream>
 #include <queue>
 using namespace std;
-int n, testCase, ans;
-int student[100002];
+
+int studentList[100002];
 bool ischosen[100002];
+int totalMem, testCase, ans;
+
+int getSuccessMem(int mem)
+{
+  int firstMem = mem;
+  bool tempChosen[100002];
+  queue<int>Q;
+  fill(tempChosen, tempChosen+totalMem+1, 0);
+  int successMem = 1;
+  int chosenMem = studentList[firstMem];
+  Q.push(chosenMem); tempChosen[chosenMem] = 1;
+  while(1){
+    int selector = Q.front(); Q.pop();
+    if(selector == firstMem) break;
+    int chosenMem = studentList[selector];
+    if(tempChosen[chosenMem] || ischosen[chosenMem]) return 0;
+    successMem++;
+    Q.push(chosenMem); tempChosen[chosenMem] = 1;
+  }
+  for(int studentNum = 1; studentNum <= totalMem; studentNum++){
+    ischosen[studentNum] =  tempChosen[studentNum];
+  }
+  return successMem;
+}
 
 int main()
 {
   ios::sync_with_stdio(0); cin.tie(0);
   cin >> testCase;
   for(int tc = 0; tc < testCase; tc++){
-    cin >> n;
-    ans = n;
-    queue<int>Q;
-    int fail = 0;
-    for(int i = 0; i < n; i++) cin >> student[i];
-    fill(ischosen, ischosen+n+1, 0);
-    for(int studentIdx = 0; studentIdx < n; studentIdx++){
-      int chosenStudent = student[studentIdx];
-      if(ischosen[chosenStudent]) continue;
-      Q.push(chosenStudent); ischosen[chosenStudent] = 1;
-      int successNum = 0;
-      while(!Q.empty()){
-        int curStudent = Q.front(); Q.pop();
-        int chosenStudent = student[curStudent];
-        if(ischosen[chosenStudent]) continue;
-        successNum++;
-      }
-      ans -= successNum;
+    cin >> totalMem;
+    int successMem = 0;
+    for(int i = 1; i <= totalMem; i++) cin >> studentList[i];
+    fill(ischosen, ischosen+totalMem+1, 0);
+    for(int studentNum = 1; studentNum <= totalMem; studentNum++){
+      if(ischosen[studentNum]) continue;
+      int result = getSuccessMem(studentNum);
+      //cout << result << ' ';
+      successMem += result;
     }
+    cout << totalMem - successMem <<'\n';
   }
   return 0;
 }
