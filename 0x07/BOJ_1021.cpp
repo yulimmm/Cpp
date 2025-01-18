99e6288f6803ebd412c76851360f92bb79f83a79
yulimmm <89307538+yulimmm@users.noreply.github.com>
2025-01-18 16:07:41 +0900

    Update BOJ_1021.cpp

--- 0x07/BOJ_1021.cpp
+++ 0x07/BOJ_1021.cpp
@@ -1,55 +1,32 @@
 #include <iostream>
-#include <cmath>
+#include <deque>
+#include <algorithm>
 using namespace std;
-bool ispiecked[52];
-int Front, End, qsize, total;
-int ans = 0;
-
-int alreadyPickedCheck()
-{
-
-}
-
-void rangeCheck()
-{
-  if(Front < 1)Front = 1;
-  if(Front > qsize) Front = qsize;
-  if(End < 1) End = 1;
-  if(End > qsize) End = qsize;
-}
-
-int judgement(int num)
-{
-  int count = 0;
-  if(num==Front) {
-    ispiecked[num] = 1;
-    Front = num+1;
-    End = Front -1
-  }
-  else if(abs(num-Front) > abs(num-End)){ //왼쪽 이동
-    count = abs(num-End) - alreadyPickedCheck();
-    Front = num-1;
-    End = Front + 1;
-    rangeCheck();
-    ispiecked[num] = 1;
-  }
-  else{ //오른쪽 이동
-
-  }
-  return count;
-}
 
 int main()
 {
   ios::sync_with_stdio(0); cin.tie(0);
+  deque<int>DQ;
+  int qsize, total, ans = 0;
   cin >> qsize >> total;
-  Front = 1; 
-  End = qsize;
+  for(int i = 1; i <= qsize; i++) DQ.push_back(i);
   while(total--){
     int num;
     cin >> num;
-    ans += judgement(num);
+    int idx = find(DQ.begin(), DQ.end(), num) - DQ.begin();
+    while(DQ.front()!=num){
+      if(idx < DQ.size()-idx){
+        DQ.push_back(DQ.front());
+        DQ.pop_front();
+      }
+      else{
+        DQ.push_front(DQ.back());
+        DQ.pop_back();
+      }
+      ans++;
+    }
+    DQ.pop_front();
   }
-  cout<<ans;
+  cout << ans;
   return 0;
 }
