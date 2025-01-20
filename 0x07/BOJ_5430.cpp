a18e6392ce44d2009db2181338d5e151a64d710f
yulimmm <89307538+yulimmm@users.noreply.github.com>
2025-01-20 15:26:50 +0900

    Update BOJ_5430.cpp

--- 0x07/BOJ_5430.cpp
+++ 0x07/BOJ_5430.cpp
@@ -1,67 +1,66 @@
 #include <iostream>
 #include <deque>
+#include <string>
+#include <sstream> // For parsing numbers
 using namespace std;
 
-void parse(string& tmp, deque<int>& d){
-  int cur = 0;
-  for(int i = 1; i+1 < tmp.size(); i++)
-  {
-    if(tmp[i] == ','){
-      d.push_back(cur);
-      cur = 0;
-    }
-    else{
-      cur = 10 * cur + (tmp[i] - '0');
-    }
-  }
-  if(cur != 0)
-    d.push_back(cur);
-}
+int testCase;
 
-void print_result(deque<int>& d){
-  cout << '[';
-  for(int i = 0; i < d.size(); i++)
-  {
-    cout << d[i];
-    if(i+1 != d.size())
-      cout << ',';
-  }
-  cout << "]\n";
-}
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> testCase;
+    
+    while (testCase--) {
+        string cmd, num;
+        int total;
+        cin >> cmd >> total >> num;
 
-int t;
-int main(){
-  ios::sync_with_stdio(0);
-  cin.tie(0);
-  cin >> t;
-  while(t--){
-    deque<int> d;
-    int rev = 0;
-    int n;
-    bool isWrong = false;
-    string query, tmp;
-    cin >> query;
-    cin >> n;
-    cin >> tmp;
-    parse(tmp, d);
-    for(char c : query)
-    {
-      if(c == 'R')
-        rev = 1 - rev;
-      else{
-        if(d.empty()){
-          isWrong = true;
-          break;
+        deque<int> dq;
+        bool front = true, err = false;
+
+        // 배열 파싱
+        num = num.substr(1, num.size() - 2); // Remove brackets '[' and ']'
+        stringstream ss(num);
+        string value;
+        while (getline(ss, value, ',')) {
+            if (!value.empty()) {
+                dq.push_back(stoi(value)); // Convert string to integer
+            }
         }
-        if(!rev) d.pop_front();
-        else d.pop_back();
-      }
+
+        // 명령어 처리
+        for (char c : cmd) {
+            if (c == 'R') {
+                front = !front; // Toggle direction
+            } else if (c == 'D') {
+                if (dq.empty()) {
+                    cout << "error\n";
+                    err = true;
+                    break;
+                }
+                if (front) dq.pop_front();
+                else dq.pop_back();
+            }
+        }
+
+        if (err) continue;
+
+        // 출력
+        cout << "[";
+        if (front) {
+            for (size_t i = 0; i < dq.size(); ++i) {
+                cout << dq[i];
+                if (i < dq.size() - 1) cout << ",";
+            }
+        } else {
+            for (size_t i = dq.size(); i > 0; --i) {
+                cout << dq[i - 1];
+                if (i > 1) cout << ",";
+            }
+        }
+        cout << "]\n";
     }
-    if(isWrong)
-      cout << "error\n";
-    else{
-      if(rev) reverse(d.begin(), d.end());
-      print_result(d);
-    }
-  }
+
+    return 0;
 }
