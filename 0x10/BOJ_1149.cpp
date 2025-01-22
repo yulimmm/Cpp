43c0ccc0a6959cece969560263e3f24a13db1169
yulimmm <89307538+yulimmm@users.noreply.github.com>
2025-01-22 17:27:13 +0900

    Update BOJ_1149.cpp

--- 0x10/BOJ_1149.cpp
+++ 0x10/BOJ_1149.cpp
@@ -1,31 +1,27 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
-
-int rgb[1000][3];
-int N;
-int d[1000][3];
+int p[1005][3]; //price
+int dp[1005][3];
+int n;
 
 int main()
 {
   ios::sync_with_stdio(0); cin.tie(0);
-  cin >> N;
-  for(int i = 1; i <= N; i++){
+  cin >> n;
+  for(int i = 1; i <= n; i++)
+    for(int j = 0; j < 3; j++) cin >> p[i][j];
+  dp[1][0] = p[1][0];
+  dp[1][1] = p[1][1];
+  dp[1][2] = p[1][2];
+  for(int i = 2; i <= n; i++){
     for(int j = 0; j < 3; j++){
-      cin >> rgb[i][j];
+      dp[i][0] = min(dp[i-1][1],dp[i-1][2]) + p[i][0];
+      dp[i][1] = min(dp[i-1][0],dp[i-1][2]) + p[i][1];
+      dp[i][2] = min(dp[i-1][0],dp[i-1][1]) + p[i][2];
     }
   }
-  //초기값
-  d[1][0] = rgb[1][0];
-  d[1][1] = rgb[1][1];
-  d[1][2] = rgb[1][2];
-  //점화식
-  for(int i = 2; i <= N; i++){
-    d[i][0] = min(d[i-1][1], d[i-1][2]) + rgb[i][0];
-    d[i][1] = min(d[i-1][0], d[i-1][2]) + rgb[i][1];
-    d[i][2] = min(d[i-1][0], d[i-1][1]) + rgb[i][2];
-  }
-  int mind = min(d[N][0],d[N][1]);
-  cout << min(mind, d[N][2]);
-
+  int ans = min(dp[n][0], dp[n][1]);
+  cout << min(ans, dp[n][2]);
   return 0;
 }
