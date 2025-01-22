7b400f5fd0474b9f91e756d1e31953f854ba4cf0
yulimmm <89307538+yulimmm@users.noreply.github.com>
2025-01-22 17:45:00 +0900

    Update BOJ_11726.cpp

--- 0x10/BOJ_11726.cpp
+++ 0x10/BOJ_11726.cpp
@@ -1,16 +1,22 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
+int d[1005];
+int n;
 
-int d[10005];
-int mod = 10007;
-
-int main(void){
-  ios::sync_with_stdio(0);
-  cin.tie(0);
-  int n;
+int main()
+{
+  ios::sync_with_stdio(0); cin.tie(0);
   cin >> n;
   d[1] = 1;
   d[2] = 2;
-  for(int i = 3; i <= n; i++) d[i] = (d[i-1]+d[i-2])%mod;
+  for(int i = 3; i <= n; i++){
+    d[i] = (d[i-1]+d[i-2]) % 10007;
+  }
   cout << d[n];
+  return 0;
 }
+//d[1] = 1 => 1
+//d[2] = 2 => 11,2
+//d[3] = 3 => 111,21,12
+//d[4] = 5 => 1111,121,211,112,22
