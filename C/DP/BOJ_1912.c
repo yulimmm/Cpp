b512035efb4978176aaa589d8e200322c97c3c09
yulimmm <89307538+yulimmm@users.noreply.github.com>
2025-01-30 16:04:45 +0900

    Update BOJ_1912.c

--- C/DP/BOJ_1912.c
+++ C/DP/BOJ_1912.c
@@ -1,31 +1,32 @@
 #include <stdio.h>
-int d[100005];
 int a[100005];
+int d[100005];
 
 int max(int a, int b)
 {
-	if(a>=b) return a;
-	else return b;
+    if(a >= b) return a;
+    return b;
 }
 
-int getElement(int num)
+int getMax(int num)
 {
-	int result;
-	for(int i = 0; i <= num; i++){
-		if(result < d[i]) result = d[i];
-	}
-	return result;
+    int max = d[1];
+    for(int i = 2; i <= num; i++){
+        if(max <= d[i]) max = d[i];
+    }
+    return max;
 }
 
 int main()
 {
-	int num;
-	scanf("%d",&num)
-	for(int i = 1; i <= num; i++) scanf("%d", a[i]);
-	for(int i = 1; i <= num; i++){
-		d[i] = max(0,d[i-1])+a[i];
-	}
-	int result = getElement(num);
-	printf("%d",&result);
-	return 0;
+    int num;
+    scanf("%d", &num);
+    for(int i = 1; i <= num; i++){
+        scanf("%d",&a[i]);
+    }
+    for(int i = 1; i <= num; i++){
+        d[i] = max(0, d[i-1]) + a[i];
+    }
+    int result = getMax(num);
+    printf("%d", result);
 }
